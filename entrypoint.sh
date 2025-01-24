2a7c9a382856a31a99a01f098d9591a0330802ac
VictoriaBob <108706985+VictoriaBob@users.noreply.github.com>
2025-01-24 15:34:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,18 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 base64 -d config > config.json
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
+VLESS_XHTTPPATH=${VLESS_XHTTPPATH:-'/path4xhttp'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
-sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
+sed -i "s#UUID#$UUID#g;s#VLESS_XHTTPPATH#${VLESS_XHTTPPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" config.json
+sed -i "s#VLESS_XHTTPPATH#${VLESS_XHTTPPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g" /etc/nginx/nginx.conf
 
-# 伪装 v2ray 执行文件
+# 伪装 xray 执行文件
 RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv v ${RELEASE_RANDOMNESS}
 cat config.json | base64 > config
 rm -f config.json
 
-# 运行 nginx 和 v2ray
+# 运行 nginx 和 xray
 nginx
 base64 -d config > config.json
 ./${RELEASE_RANDOMNESS} run
